d4a6cdc9f53d801d03c2a979619aa47a2e1f211f
Samuel Villa Valencia <svillav4@eafit.edu.co>
2025-03-11 12:30:06 -0500

    Made some changes in services views.py

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HomePageView, ShowServiceView, CreateServiceView, ServiceSuccessView
+from .views import HomePageView, MyServicesView, CreateServiceView, ServiceSuccessView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path('services/', ShowServiceView.as_view(), name='show_services'),
+    path('services/', MyServicesView.as_view(), name='my_services'),
     path('services/create', CreateServiceView.as_view(), name='create_service'),
     path('services/success', ServiceSuccessView.as_view(), name='service_success'),
 ]
--- services/views.py
+++ services/views.py
@@ -4,42 +4,42 @@ from django.views.generic import TemplateView
 from django.views import View
 from .forms import ServiceForm
 from .models import Service
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
-class ShowServiceView(View):
-    template_name = 'services/show_services.html'
+class MyServicesView(View):
+    template_name = 'services/my_services.html'
 
     def get(self, request,):
         services = Service.objects.all()
         return render(request, self.template_name, {"services": services})
 
 
-class CreateServiceView(TemplateView):
+class CreateServiceView(LoginRequiredMixin, TemplateView):
     template_name = 'services/create_service.html'
 
-    # @login_required
-    def get(self, request, *args, **kargs):
-        #if not request.user.is_freelancer:  # Validación con el booleano
-            #return HttpResponse("No tienes permisos para crear un servicio.", status=403)
-        
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'freelancer'):
+            return HttpResponse("No tienes permisos para crear un servicio.", status=403)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
         form = ServiceForm()
-        return render(request, self.template_name, {'form' : form})
-    
-    # @login_required
+        return render(request, self.template_name, {'form': form})
+
     def post(self, request, *args, **kwargs):
-        #if not request.user.is_freelancer:  # Validación con el booleano
-            #return HttpResponse("No tienes permisos para crear un servicio.", status=403)
-         
         form = ServiceForm(request.POST, request.FILES)
         if form.is_valid():
+            freelancer = request.user.freelancer
             service = form.save(commit=False)
-            #service.freelancer = Freelancer.objects.get(user=request.user)
+            service.freelancer = freelancer
             service.save()
             return redirect('service_success')
+        
         return render(request, self.template_name, {'form': form})
 
 class ServiceSuccessView(TemplateView):
