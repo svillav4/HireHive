e7fc536c0ef1e0108a254690d06107ea42020fc6
Samuel Villa Valencia <svillav4@eafit.edu.co>
2025-03-10 21:23:28 -0500

    start of US02 development

--- services/urls.py
+++ services/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import HomePageView, ShowServiceView, CreateServiceView, ServiceSuccessView
 
 urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path('services/', ShowServiceView.as_view(), name='show_services'),
+    path('services/create', CreateServiceView.as_view(), name='create_service'),
+    path('services/success', ServiceSuccessView.as_view(), name='service_success'),
 ]
