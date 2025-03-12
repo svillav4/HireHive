bed98d52a5ea53ffb5dd343d3c317caff524c7c6
Samuel Villa Valencia <svillav4@eafit.edu.co>
2025-03-11 20:35:20 -0500

    Finished the service creation system for freelancers (user story 02) and start of the service management system.

--- hirehive_project/urls.py
+++ hirehive_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from services.views import HomePageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,3 +26,6 @@ urlpatterns = [
     path("", include("services.urls")),
     path("", include("payments.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
