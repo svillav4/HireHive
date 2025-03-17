374e2b44667e8c52948b169d772e439a316024be
juan jose botero garcía <jjboterog@eafit.edu.co>
2025-03-16 20:05:12 -0500

    Created two sections where the freelancer and client can see the orders status

--- payments/urls.py
+++ payments/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import JobApplicationView, MyHiresView
 
 urlpatterns = [
+    path('job_application/<int:service_id>/', JobApplicationView.as_view(), name='job_application'),
+    path('my_hires/', MyHiresView.as_view(), name='my_hires'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order
 
 # Create your views here.
+
+STATUS_COLORS = {
+    'Completed': 'text_completed',
+    'Cancelled': 'text_cancelled',
+    'Pending Approval': 'text_pending_approval',
+    'In Progress': 'text_in_progress',
+}
+
+class JobApplicationView(LoginRequiredMixin, View):
+    template_name = 'job_application.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'freelancer'):
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, service_id):
+        jobApplications = Order.objects.filter(service_id = service_id).order_by('-creation_date')
+        for jobApplication in jobApplications:
+            jobApplication.status_color = STATUS_COLORS.get(jobApplication.status, '')
+        return render(request, self.template_name, {'jobApplications': jobApplications})
+
+ 
+class MyHiresView(LoginRequiredMixin, View):
+    template_name = 'my_hires.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'client'):
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        orders = Order.objects.filter(client__user=request.user.id).order_by('-creation_date')
+        for order in orders:
+            order.status_color = STATUS_COLORS.get(order.status, '')
+        return render(request, self.template_name, {'orders': orders})
